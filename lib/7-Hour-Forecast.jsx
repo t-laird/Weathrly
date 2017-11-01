bd3c328cf44334557731d057259d4629d663a92f
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-11-01 16:56:49 -0600

    Refactored the card render to stop hardcoding cards

---
+++ lib/7-Hour-Forecast.jsx
@@ -0,0 +1,29 @@
+import '../Styles/7-Hour-Forecast.scss';
+import React, { Component } from 'react';
+import Card from './Card';
+
+class SevenHourForecast extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+
+    console.log(this.props.sevenHour);
+
+    return (
+      <div className="SevenHour">
+      {this.props.sevenHour.map(hour => {
+        console.log('Hello');
+        return (
+        <Card hourWeather={hour} />
+        )
+      })
+      }
+      </div>
+    )
+  }
+}
+
+export default SevenHourForecast;
