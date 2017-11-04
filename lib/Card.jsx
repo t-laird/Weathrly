fdcf436e2d52b8ab95312af7c40b0d26b52500ff
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-11-04 12:20:34 -0600

    Made changes to the styles to match comp

--- lib/Card.jsx
+++ lib/Card.jsx
@@ -10,19 +10,28 @@ export default class Card extends React.Component {
 
   render () {
   
-    // console.log(this.props.weather)
+    console.log(this.props.weather)
 
     return (
       <div className="Card">
         {this.props.weather.high &&
           <div> 
-          <h1>{this.props.weather.day} </h1>
-          <img src={this.props.weather.icon_url} />
-          <div> {this.props.weather.condition} </div>
-          <div>High of  {this.props.weather.high}˚ </div>
-          <div>Low of  {this.props.weather.low}˚ </div>
-          <div>Min Humidity {this.props.weather.minHumid}% </div>
-          <div>Max Humidity {this.props.weather.maxHumid}% </div>
+          <h1 className="ten-day-day">{this.props.weather.day} </h1>
+          <img className="ten-day-icon" src={this.props.weather.icon_url} />
+          <div className="ten-day-condition"> {this.props.weather.condition} </div>
+          <hr></hr>
+          <div className="ten-day-temp-wrap">
+          <div className="ten-day-temp-high"> {this.props.weather.high}˚ </div>
+          <div className="ten-day-temp-low"> {this.props.weather.low}˚ </div>
+          </div>
+          <div className="ten-day-humid-wrap">
+          <div className="ten-day-humid-high"> Humid {this.props.weather.maxHumid}% </div>
+          <div className="ten-day-humid-low"> {this.props.weather.minHumid}% </div>
+          </div>
+          <div className="ten-day-wind-wrap">
+          <div className="ten-day-wind-speed"> Wind {this.props.weather.maxWind}mph</div>
+          <div className="ten-day-wind-direction"> {this.props.weather.windDirection}</div>
+          </div>
           </div>
         }
         {!this.props.weather.high &&
