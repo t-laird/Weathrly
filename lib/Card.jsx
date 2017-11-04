c550a21a63364b68b9cc53e819a8b42bf0caf861
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-11-03 18:00:51 -0600

    Fixed our syntax so that the cards render again

--- lib/Card.jsx
+++ lib/Card.jsx
@@ -1,50 +1,29 @@
 import '../Styles/Card.scss';
 import React from 'react';
 
+
 export default class Card extends React.Component {
   constructor () {
     super ();
 
   }
+
   render () {
+  
+    console.log(this.props.weather.condition)
+
     return (
       <div className="Card">
-      <div className="index"> {this.props.index} </div>
-
-        {
-          this.props.sevenHour &&
-          <span className="seven-hour-top-row">
-            <h1 className="seven-hour-hour">
-            {this.props.hourWeather.hour}</h1>
-            <h1 className="seven-hour-day">
-            {this.props.hourWeather.day}</h1>
-          </span>
-        }
-        {
-          !this.props.sevenHour &&
-          <span className="ten-day-top-row">
-            <h1 className="ten-day-week-day">
-            {this.props.dayWeather.weekday}</h1>
-            <h1 className="ten-day-month-day">
-            {this.props.dayWeather.monthname_short}, {this.props.dayWeather.day}</h1>
-          </span>
-        }
-          <p>
-          <span className="seven-hour-icon"><img src={this.props.hourWeather.icon_url}>
-            </img> </span> <br />
-            <span className="seven-hour-condition">
-            {this.props.hourWeather.condition} 
-          </span><br />
-          <span className="seven-hour-bottom-row">
-            <span className="seven-hour-temp">
-            Temp {this.props.hourWeather.temp.english}˚ </span> <br />
-            <span className="seven-hour-wind-deg">
-            Wind {this.props.hourWeather.windDirection.dir} at {this.props.hourWeather.windSpeed.english}mph </span> <br />
-            <span className="seven-hour-wind-speed"></span> <br />
-            <span className="seven-hour-humidity">
-            Humidity {this.props.hourWeather.humidity}% </span> <br />
-          </span>
-          </p>
+        <h1>Hour {this.props.weather.hour} </h1>
+        <h1>{this.props.weather.day} </h1>
+        <img src={this.props.weather.icon_url} />
+        <div> {this.props.weather.condition} </div>
+        <div>High Temp {this.props.weather.high}˚ </div>
+        <div>Low Temp {this.props.weather.low}˚ </div>
+        <div>Humidity {this.props.weather.humidity}% </div>
+        <div>Wind Direction {this.props.weather.windDirection} </div>
+      {/*
+      */}
       </div>
     )
   }
