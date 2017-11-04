ef83447ef819ce790ac8e0a358800f22cb311af4
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-11-03 19:12:02 -0600

    Figured out conditional rendering!

--- lib/Card.jsx
+++ lib/Card.jsx
@@ -10,21 +10,29 @@ export default class Card extends React.Component {
 
   render () {
   
-    console.log(this.props.weather)
+    // console.log(this.props.weather)
 
     return (
       <div className="Card">
-        <h1>Hour {this.props.weather.hour} </h1>
+        <h1>{this.props.weather.hour} </h1>
         <h1>{this.props.weather.day} </h1>
         <img src={this.props.weather.icon_url} />
         <div> {this.props.weather.condition} </div>
-        <div>High Temp {this.props.weather.high}˚ </div>
-        <div>Low Temp {this.props.weather.low}˚ </div>
-        <div>Humidity {this.props.weather.humidity}% </div>
-        <div>Wind Direction {this.props.weather.windDirection} </div>
-      {/*
-        <div>Wind Speed {this.props.weather.windSpeed} </div>
-      */}
+        {this.props.weather.high &&
+          <div> 
+          <div>High of  {this.props.weather.high}˚ </div>
+          <div>Low of  {this.props.weather.low}˚ </div>
+          <div>Min Humidity {this.props.weather.minHumid}% </div>
+          <div>Max Humidity {this.props.weather.maxHumid}% </div>
+          </div>
+        }
+        {!this.props.weather.high &&
+          <div>
+          <div>Temp {this.props.weather.temp}˚ </div>         
+          <div>Humidity {this.props.weather.humidity}% </div>
+          </div>
+        }
+        <div>Wind {this.props.weather.windDirection} </div>
       </div>
     )
   }
