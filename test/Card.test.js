17fbd744cf99fae606857a1d80a537532d474757
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-11-05 16:23:18 -0700

    unskipped all of the tests to check what is passing and removed console.logs

--- test/Card.test.js
+++ test/Card.test.js
@@ -14,7 +14,7 @@ describe('Card', ()=> {
     expect(wrapper).toBeDefined();
   });
 
-  it ('Should receive a props object with hour data when passed hour data', () => {
+  it('Should receive a props object wit.skip hour data when passed hour data', () => {
     const wrapper = mount(<Card weather={hourCardData[0]} />);
     expect(wrapper.props().weather.hour).toBeDefined();
     expect(wrapper.props().weather.temp).toBeDefined();
@@ -23,7 +23,7 @@ describe('Card', ()=> {
     expect(wrapper.props().weather.humidity).toBeDefined();
   });
 
-  it ('Should receive a props object with day data when passed day data', () => {
+  it('Should receive a props object with day data when passed day data', () => {
     const wrapper = mount(<Card weather={dayCardData[0]} />);
     expect(wrapper.props().weather.condition).toBeDefined();
     expect(wrapper.props().weather.day).toBeDefined();
@@ -31,7 +31,7 @@ describe('Card', ()=> {
     expect(wrapper.props().weather.low).toBeDefined();
   });
 
-  it ('Should render a different set of html when given day data', () => {
+  it('Should render a different set of html when given day data', () => {
     const wrapper = mount(<Card weather={dayCardData[0]} />);
     expect(wrapper.find('div').slice(3, 4).text().includes('High of')).toEqual(true);
     expect(wrapper.find('div').slice(4, 5).text().includes('Low of')).toEqual(true);
@@ -39,19 +39,19 @@ describe('Card', ()=> {
     expect(wrapper.find('div').slice(6, 7).text().includes('Max Humidity')).toEqual(true);
   });
 
-  it ('Should not render any of the day data when given hour data', () => {
+  it('Should not render any of the day data when given hour data', () => {
     const wrapper = mount(<Card weather={hourCardData[0]} />);
     expect(wrapper.find('div').slice(3, 4).text().includes('High of')).toEqual(false);
     expect(wrapper.find('div').slice(4, 5).text().includes('Low of')).toEqual(false);
   });
 
-  it ('Should render a different set of html when given hour data', () => {
+  it('Should render a different set of html when given hour data', () => {
     const wrapper = mount(<Card weather={hourCardData[0]} />);
     expect(wrapper.find('div').slice(2, 3).text().includes('Temp')).toEqual(true);
     expect(wrapper.find('div').slice(3, 4).text().includes('Humidity')).toEqual(true);
   });
   
-  it ('Should not render any of the hour data when given day data', () => {
+  it('Should not render any of the hour data when given day data', () => {
     const wrapper = mount(<Card weather={dayCardData[0]} />);
     expect(wrapper.find('div').slice(2, 3).text().includes('Temp')).toEqual(false);
     expect(wrapper.find('div').slice(3, 4).text().includes('Humidity')).toEqual(false);
