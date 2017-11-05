bb6b40a8a9e1d6faebbd9634ca4f361ae3de328e
Thomas Laird <t6r6l5@gmail.com>
2017-11-05 16:46:33 -0700

    unskip 10 day

--- test/10-Day-Forecast.test.js
+++ test/10-Day-Forecast.test.js
@@ -8,47 +8,47 @@ let testData = cleanData(apiData);
 
 
 describe('10 Day' , () => {
-  it.skip('should exist', () => {
+  it('should exist', () => {
     const wrapper = shallow(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(wrapper).toBeDefined();
   });
 
-  it.skip ('Should initialize 10 card components', () => {
+  it ('Should initialize 10 card components', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(wrapper.find('.Card').length).toEqual(10);
   });
 
-  it.skip ('Should receive expected number of props', () => {
+  it ('Should receive expected number of props', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(Object.keys(wrapper.props().tenDay[0]).length).toEqual(13);
   });
 
-  it.skip ('Should receive "Partly Cloudy" as the prop for condition', () => {
+  it ('Should receive "Partly Cloudy" as the prop for condition', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(wrapper.props().tenDay[0].condition).toEqual('Partly Cloudy');
   });
 
-  it.skip ('Should receive "SSE" as the prop for condition windCondition', () => {
+  it ('Should receive "SSE" as the prop for condition windCondition', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(wrapper.props().tenDay[0].windDirection).toEqual('SSE');
   });
 
-  it.skip ('The first card component should have a day of Tuesday', () => {
+  it ('The first card component should have a day of Tuesday', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />)
     expect(wrapper.find('.Card').find('h1').slice(0,1).text()).toEqual('Tuesday ');
   });
 
-  it.skip ('The last card component should have a day of Sunday', () => {
+  it ('The last card component should have a day of Sunday', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />)
     expect(wrapper.find('.Card').find('h1').slice(5,6).text()).toEqual('Sunday ');
   });
 
-  it.skip ('The first card component should have a low of 43 degrees', () => {
+  it ('The first card component should have a low of 43 degrees', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(wrapper.find('.Card').slice(0,1).find('div').slice(4,5).text()).toEqual(' 54˚  43˚ ');
   });
 
-  it.skip ('The last card component should have a low of 41 degrees', () => {
+  it ('The last card component should have a low of 41 degrees', () => {
     const wrapper = mount(<TenDayForecast tenDay= {testData.tenDayObject} />);
     expect(wrapper.find('.Card').slice(5,6).find('div').slice(4,5).text()).toEqual(' 68˚  41˚ ');
   });
