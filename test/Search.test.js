7d82410362c069b853a5cada197f427621e1d58b
Thomas Laird <t6r6l5@gmail.com>
2017-11-06 13:40:08 -0700

    Set up search tests

---
+++ test/Search.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Search from '../lib/Search';
+
+describe('Search', () => {
+  it('Should create a search component', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('')
+
+});
