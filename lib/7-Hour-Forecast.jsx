8ef54dce592035b218058cc3bcad1718f95eefcf
Thomas Laird <t6r6l5@gmail.com>
2017-11-06 21:15:04 -0700

    change stateful components where state is not used to stateless components

--- lib/7-Hour-Forecast.jsx
+++ lib/7-Hour-Forecast.jsx
@@ -1,32 +1,23 @@
 import '../Styles/7-Hour-Forecast.scss';
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
 
-class SevenHourForecast extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-
-    return (
-      <div className="SevenHour">
-        <div className="seven-hour-container-title">7 Hour Forecast</div>
-        <div className="card-container">
-          {
-            this.props.sevenHour.map((hour, index) => {
-              return (
-                <Card weather={hour} key={index} />
-              );
-            })
-          }
-        </div>
+function SevenHourForecast (props) {
+  return (
+    <div className="SevenHour">
+      <div className="seven-hour-container-title">7 Hour Forecast</div>
+      <div className="card-container">
+        {
+          props.sevenHour.map((hour, index) => {
+            return (
+              <Card weather={hour} key={index} />
+            );
+          })
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 SevenHourForecast.propTypes = {
--- lib/Current-Weather.jsx
+++ lib/Current-Weather.jsx
@@ -1,50 +1,42 @@
 import '../Styles/Current-Weather.scss';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class CurrentWeather extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+function CurrentWeather (props) {
+  //eslint-disable-next-line max-len
+  let { location, tempNum, high, low, windDegs, windDir, windSpeed, observationTime, sentence, icon } = props.currentWeather;
 
-  render() {
-    //eslint-disable-next-line max-len
-    let { location, tempNum, high, low, windDegs, windDir, windSpeed, observationTime, sentence, icon } = this.props.currentWeather;
+  icon = `wi wi-wu-${icon.replace(/\s/g, '')}`;
+  icon === 'wi wi-wu-fog' ? icon = 'wi wi-day-fog' : null;
 
-    icon = `wi wi-wu-${icon.replace(/\s/g, '')}`;
-    icon === 'wi wi-wu-fog' ? icon = 'wi wi-day-fog' : null;
-
-    return (
-      <div className="CurrentWeather">
-        <div className="leftHalf">
-          <h1>{location}</h1>
-          <h4>{observationTime}</h4>
-          <div>
-            <i className={icon}></i>
-            <h1>{tempNum}˚</h1>
-          </div>
-          <p className = "wind">
-            Wind {windDir} - {windSpeed} mph 
-            <i className={"wi wi-wind towards-" + windDegs + "-deg"}></i>
-          </p>
+  return (
+    <div className="CurrentWeather">
+      <div className="leftHalf">
+        <h1>{location}</h1>
+        <h4>{observationTime}</h4>
+        <div>
+          <i className={icon}></i>
+          <h1>{tempNum}˚</h1>
         </div>
-        <div className="divider">
-
-        </div>
-        <div className="rightHalf">
-          <h3>TODAY:</h3>
-          <span className="high">{high}˚ </span> 
-          <span className="low">{low}˚ </span>
-          <p>
-            {sentence}
-          </p>
-        </div>  
+        <p className = "wind">
+          Wind {windDir} - {windSpeed} mph 
+          <i className={"wi wi-wind towards-" + windDegs + "-deg"}></i>
+        </p>
       </div>
-    );
-  }
-}
+      <div className="divider">
 
+      </div>
+      <div className="rightHalf">
+        <h3>TODAY:</h3>
+        <span className="high">{high}˚ </span> 
+        <span className="low">{low}˚ </span>
+        <p>
+          {sentence}
+        </p>
+      </div>  
+    </div>
+  );
+}
 
 CurrentWeather.propTypes = {
   currentWeather: PropTypes.object
