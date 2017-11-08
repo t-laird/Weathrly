bf4e51fce8da35b6aac2a2e3cc01be9aa7d3e59f
Thomas Laird <t6r6l5@gmail.com>
2017-11-07 17:14:32 -0700

    refactor welcome component to stateless

--- lib/Welcome.jsx
+++ lib/Welcome.jsx
@@ -1,38 +1,15 @@
 import '../Styles/Welcome.scss';
-import React, { Component } from 'react';
+import React from 'react';
 import Search from './Search';
 import PropTypes from 'prop-types';
 
-class Welcome extends Component {
-  constructor() {
-    super();
-    this.hideWelcome = this.hideWelcome.bind(this);
-    this.state = {display: 'default', value: ''};
-    this.updateVal = this.updateVal.bind(this);
-    this.setLocation = this.setLocation.bind(this);
-  }
-
-  hideWelcome() {
-    this.setState({display: 'none'});
-  }
-
-  updateVal(e) {
-    this.setState({value: e.target.value});
-  }
-
-  setLocation() {
-    this.props.updateFunction(this.state.value);
-    this.setState({value: ''});
-  }
-
-  render() {
-    return (
-      <div className="Welcome" style={{'display': this.state.display}}>
-        <Search updateFunction={this.props.updateFunction}/>
-        <h3>Welcome to Weathrly! <i className="wi wi-day-sunny"></i></h3>
-      </div>
-      );
-  }
+function Welcome (props) {
+  return (
+    <div className="Welcome">
+      <Search updateFunction={props.updateFunction}/>
+      <h3>Welcome to Weathrly! <i className="wi wi-day-sunny"></i></h3>
+    </div>
+  );
 }
 
 Welcome.propTypes = {
